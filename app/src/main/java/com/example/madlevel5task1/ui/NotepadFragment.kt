a31a945b2bca01718acc98aac57697c862fd1794
KevinHooij <kevin.hooijschuur@hva.nl>
2020-11-24 00:33:18 +0100

    Completes step 4 and 5. App done

--- app/src/main/java/com/example/madlevel5task1/ui/NotepadFragment.kt
+++ app/src/main/java/com/example/madlevel5task1/ui/NotepadFragment.kt
@@ -1,18 +1,31 @@
 package com.example.madlevel5task1.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel5task1.R
+import com.example.madlevel5task1.model.Note
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_add_note.*
+import kotlinx.android.synthetic.main.fragment_notepad.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class NotepadFragment : Fragment() {
+
+    private val viewModel: NoteViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -25,5 +38,23 @@ class NotepadFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        observeAddNoteResult()
     }
+
+
+
+    private fun observeAddNoteResult() {
+        viewModel.note.observe(viewLifecycleOwner, Observer{ note ->
+            note?.let {
+                tvNoteTitle.text = it.noteTitle
+                tvDateLastUpdate.text = it.date.toString()
+                tvNoteText.text = it.noteText
+            }
+        })
+    }
+
+
+
+
 }
